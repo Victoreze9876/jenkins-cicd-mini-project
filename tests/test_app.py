a6f8c1e1b67559b01691153c374cbd3b8db34dcd
VICTOR <victoreze9876@gmail.com>
2026-09-02 14:41:15 +0100

    Create test failure for Jenkins troubleshooting

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ from src.app import add, subtract, multiply, divide
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 6)
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 2), 3)
